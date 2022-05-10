2d51b37a9a90f9f28fddbb6d41104bcbf2543c64
shalini9990 <100773238+shalini9990@users.noreply.github.com>
2022-05-11 01:48:39 +0800

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -3,6 +3,7 @@ let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
     shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
 
@@ -11,6 +12,7 @@ let shoppingCart = document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick = () =>{
     shoppingCart.classList.toggle('active');
     searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
 
@@ -20,11 +22,13 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
     shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
 }
 
 window.onscroll = () =>{
     searchForm.classList.remove('active');
     shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
     navbar.classList.remove('active');
 }
 
@@ -48,3 +52,4 @@ var swiper = new Swiper(".product-slider", {
       },
     },
 });
+
